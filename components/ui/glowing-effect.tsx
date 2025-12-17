c92d09cd8a51b730f1342522083d448cf9620b07
SwayamWakodikar <swayam.w06@gmail.com>
2025-12-17 16:09:47 +0530

    ---

--- components/ui/glowing-effect.tsx
+++ components/ui/glowing-effect.tsx
@@ -124,7 +124,7 @@ const GlowingEffect = memo(
             "pointer-events-none absolute -inset-px hidden rounded-[inherit] border opacity-0 transition-opacity",
             glow && "opacity-100",
             variant === "white" && "border-white",
-            disabled && "!block"
+            disabled && "block!"
           )}
         />
         <div
@@ -161,9 +161,9 @@ const GlowingEffect = memo(
           className={cn(
             "pointer-events-none absolute inset-0 rounded-[inherit] opacity-100 transition-opacity",
             glow && "opacity-100",
-            blur > 0 && "blur-[var(--blur)] ",
+            blur > 0 && "blur-(--blur) ",
             className,
-            disabled && "!hidden"
+            disabled && "hidden!"
           )}
         >
           <div
