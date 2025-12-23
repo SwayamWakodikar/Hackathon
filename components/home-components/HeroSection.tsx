1a7a4ed41a9220190fce9fce73ba0df4d43c831c
SwayamWakodikar <swayam.w06@gmail.com>
2025-12-23 17:27:55 +0530

    nothing much

--- components/home-components/HeroSection.tsx
+++ components/home-components/HeroSection.tsx
@@ -7,7 +7,7 @@ interface HeroSectionProps {
 export default function HeroSection({ isDark }: HeroSectionProps) {
   return (
     <div className="container mx-auto px-6 py-12 text-center">
-      <h2 className="text-5xl text-black font-bold mb-4 text-foreground dark:text-white ">
+      <h2 className="text-5xl font-bold mb-4 text-foreground dark:text-white ">
         Build Your Perfect Resume with AI
       </h2>
       <p className="text-xl mb-8 text-muted-foreground">
