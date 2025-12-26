782f5c26fdc80fcea2e483cf13a1adae3ea678be
SwayamWakodikar <swayam.w06@gmail.com>
2025-12-26 12:53:42 +0530

    added redirection buttons

--- components/Register.tsx
+++ components/Register.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import { signIn } from "next-auth/react";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
@@ -65,7 +66,11 @@ export default function Register() {
           </button>
           <button
             className="group/btn shadow-input relative flex h-10 w-full items-center justify-start space-x-2 rounded-md bg-gray-50 px-4 font-medium text-black dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_#262626]"
-            type="submit"
+            type="submit" onClick={() =>
+                          signIn("google", {
+                            callbackUrl: "/resume/home",
+                          })
+                        }
           >
             <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
             <span className="text-sm text-neutral-700 dark:text-neutral-300">
@@ -111,3 +116,7 @@ const LabelInputContainer = ({
     </div>
   );
 };
+const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Form submitted");
+  };
